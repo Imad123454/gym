02261b687ad80b0cf493221464b72edb009f22a6
Imad123454 <baigimad3@gmail.com>
2025-12-26 10:28:36 +0530

    push code

--- gms/settings.py
+++ gms/settings.py
@@ -93,12 +93,8 @@ WSGI_APPLICATION = "gms.wsgi.application"
 # =========================
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "fit",
-        "USER": "root",
-        "PASSWORD": "imad1234",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
